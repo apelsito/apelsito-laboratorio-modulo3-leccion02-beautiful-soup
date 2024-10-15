34d39d5e1de7268a3a4ba3a15594a9350265d3a5
apelsito <mrappletastico@gmail.com>
2024-10-15 17:41:50 +0200

    Doing Functions

--- src/SupportLab3.py
+++ src/SupportLab3.py
@@ -21,8 +21,45 @@ def web_response(url):
     else:
         print(f"Error, respuesta recibida: {code}")
         print(f"Ha fallado la url: {url}")
-        return None
+        return np.nan
 
 def convert_to_soup(tosoupcontent):
     soup = BeautifulSoup(tosoupcontent.content, "html.parser")
     return soup
+
+def obtain_elements(searchresultlist):
+    element_operation = [element.getText() for element in searchresultlist]
+    return element_operation
+
+def format_dimension(dimension):
+    operation = dimension.replace("\n","").replace(" (cm)","")
+    return operation
+
+search= 'a",{"class":"title"}'
+def obtain_name(soup,search):
+    lista_nombre_atrezo = soup.findAll(search)
+    nombre_atrezo = obtain_elements(lista_nombre_atrezo)
+    return nombre_atrezo
+
+search= '"a",{"class":"tag"}'
+def obtain_category(soup,search):
+    lista_categoria_atrezo = soup.findAll(search)
+    categoria_atrezo = obtain_elements(lista_categoria_atrezo)
+    return categoria_atrezo
+
+def obtain_section(soup,search):
+    return
+
+search = '"div",{"class":"article-container style-1"}'
+def obtain_description(soup,search):
+    lista_descripcion_atrezo = soup.findAll(search)
+    descripcion_atrezo = obtain_elements(lista_descripcion_atrezo)
+    return descripcion_atrezo
+
+search = '"div",{"class":"price"}'
+def obtain_dimensions(soup,search):
+    lista_dimensiones_atrezo = soup.findAll(search)
+    dimensiones_atrezo = obtain_elements(lista_dimensiones_atrezo)
+    dimensiones = list(map(format_dimension,dimensiones_atrezo))
+    return dimensiones
+    
